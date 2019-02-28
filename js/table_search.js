e6a5e789b7fc7eb49a685c7901a77be4ab763d34
Michael <azhain@gmail.com>
2019-02-28 12:18:39 -0500

    fixed lists

--- js/table_search.js
+++ js/table_search.js
@@ -6,29 +6,32 @@ function search_table() {
     input = document.getElementsByClassName("search_input")[0];
     filter = input.value.toUpperCase();
     table = document.getElementsByClassName("searchable")[0];
-    tr = table.getElementsByTagName("tr");
     
     // Loop through all table rows, and hide those who don't match the search query
+    tr = table.getElementsByTagName("tr");
+
     for (row = 0; row < tr.length; row++) {
-        show = false;
-        t_cols = tr[row].getElementsByTagName("td");
+        if (tr[row].id != "table_sep_tr") {
+            show = false;
+            t_cols = tr[row].getElementsByTagName("td");
 
-        for (col = 0; col < t_cols.length; col++) {
-            td_element = t_cols[col];
+            for (col = 0; col < t_cols.length; col++) {
+                td_element = t_cols[col];
 
-            if (td_element && !show) {
-                td_value = td_element.textContent || td_element.innerText;
+                if (td_element && !show) {
+                    td_value = td_element.textContent || td_element.innerText;
 
-                if (td_value.toUpperCase().indexOf(filter) > -1) {
-                    show = true;
+                    if (td_value.toUpperCase().indexOf(filter) > -1) {
+                        show = true;
+                    }
                 }
             }
-        }
 
-        if (show) {
-            tr[row].style.display = "";
-        } else {
-            tr[row].style.display = "none";
+            if (show) {
+                tr[row].style.display = "";
+            } else {
+                tr[row].style.display = "none";
+            }
         }
     }
 }
